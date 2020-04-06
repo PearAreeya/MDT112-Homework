15c181244e131c46083acfea2dab9524ccb28c54
PearAreeya <61303454+PearAreeya@users.noreply.github.com>
2020-04-06 19:29:54 +0700

    Add files via upload

---
+++ Assignment 4/4-2/4-2.ino
@@ -0,0 +1,23 @@
+String yourAreYoung(int age) 
+{
+  String result;
+
+  result = "I'm" + String(age)+" year old ";
+
+   if (age <= 18 && age >0) 
+{
+        result= "I'm" + String(age)+" year old\n"
+
+        "Your are young."; 
+}
+
+  return result;
+
+}
+#define CURRENT_YEAR 2020
+int yourAge(int yearInCE) 
+{
+    yearInCE = CURRENT_YEAR - yearInCE;
+
+    return yearInCE;
+}
