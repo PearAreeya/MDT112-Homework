a994ee2725d36c8dc6ff0e6841c769848b2e972d
PearAreeya <61303454+PearAreeya@users.noreply.github.com>
2020-04-22 21:59:53 +0700

    Add files via upload

---
+++ 60080501622-Q1-MDT112/60080501622-Q1-MDT112/60080501622-Q1-MDT112.ino
@@ -0,0 +1,80 @@
+void setup(){
+    Serial.begin(9600); 
+    pinMode(2,INPUT_PULLUP); 
+    pinMode(3,OUTPUT);
+    pinMode(4,OUTPUT);
+    pinMode(5,OUTPUT);
+    pinMode(6,OUTPUT);
+    pinMode(7,OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(9,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(11,OUTPUT);
+    pinMode(12,OUTPUT);
+    pinMode(13,OUTPUT);
+    for(int led = 3 ; led <= 13 ;led++)
+    {
+
+        pinMode(led,OUTPUT);
+
+    }
+
+    tone(8,200,500); 
+    delay(250); 
+    tone(8,400,100); 
+    
+}
+ 
+    int l = 3;
+    int t = 100;
+    int i = 1;
+
+
+void loop(){
+  
+    for(l= 3 ; l <= 13 ; l++){ 
+      
+        if(l == 8){
+
+            digitalWrite(l,0);
+            continue;
+
+        }
+
+        digitalWrite(l,1); 
+        delay(t);
+        digitalWrite(l,0); 
+
+        if ( digitalRead(2) == 0 ){
+          
+            i = i * 2;
+            t = t / 2;
+            Serial.println("Faster X2 (Current Speed : X" + String(i) + ")");
+            tone(8,400,100);
+            delay(700);
+        }
+    }
+
+    for(l = 13 ; l >= 3 ; l--){
+
+        if(l == 8){
+
+            digitalWrite(l,0); 
+            continue;
+
+        }
+
+        digitalWrite(l,1); 
+        delay(t);
+        digitalWrite(l,0); 
+
+        if ( digitalRead(2) == 0 ){
+
+            i = i * 2;
+            Serial.println("Faster X2 (Current Speed : X" + String(i) + ")");
+            tone(8,400,100);
+            t = t / 2;
+            delay(700);
+        }
+    }
+}
